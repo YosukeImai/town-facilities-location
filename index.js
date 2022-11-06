cfd6b0587c3c37f5415e496f1db717d1d340db44
Yosuke Imai <yosuke.imai.00@gmail.com>
2022-11-06 10:39:51 +0900

    Check connection to DB at startup

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const app = require('./express/app');
-//const sequelize = require('./sequelize');
+const sequelize = require('./sequelize');
 const PORT = 3000;
+
+async function assertDatabaseConnectionOK(){
+    console.log(`Checking database connection...`);
+
+    try{
+        await sequelize.authenticate();
+        console.log(`Database connection OK!`);
+    }catch(error){
+        console.log(`Unable to connect to the database:`);
+        console.log(error.message);
+        process.exit(1);
+    }
+}
 
 async function init() {
     console.log(`Starting TOWN FACILITIES LOCATION on port ${PORT}...`);
